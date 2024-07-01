aa46344aedad916b3d8d28ac8cfdd262b618aaa8
Nghia Trung <nghia250803@gmail.com>
2024-07-02 01:22:49 +0700

    Include Chapter 3 Fundamental

---
+++ Chap3_Fundamental/LotteryArray.java
@@ -0,0 +1,32 @@
+package Chap3_Fundamental;
+/**
+ * This program demonstrates a triangular array
+ * @version 1.0 2024-07-02
+ * @author Neekon
+ */
+public class LotteryArray {
+    public static void main(String[] args)
+    {
+        final int NMAX = 10;
+        int[][] odds = new int[NMAX+1][];
+        for (int n =0; n <= NMAX; n++)
+         odds[n] = new int[n+1];
+        for(int n =0; n< odds.length;++n)
+         for(int k =0;k<odds[n].length;++k)
+         {
+            int lotteryOdds = 1;
+            for (int i =1; i<= k;++i)
+            lotteryOdds = lotteryOdds * (n-i+1)/i;
+    
+            odds[n][k] = lotteryOdds;
+         }
+         for(int[] row: odds)
+         {
+            for(int odd: row)
+              System.out.printf("%4d", odd);
+              System.out.println();
+         }
+    }
+    
+
+}
